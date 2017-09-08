bda5ceee20aeb0daa507f49d3cd10b10158361d0
Charismaforever <charismaforever023@yahoo.com>
2017-09-08 11:11:18 -0700

    Additional Changes CSS, JS
    Update to files

---
+++ apps.js
@@ -0,0 +1,5 @@
+// JavaScript Document
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
